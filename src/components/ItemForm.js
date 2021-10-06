790e648d44f206583c9c71478cf0a376e6251d1d
Alessandra Chardon <achardon@ucla.edu>
2021-10-06 09:17:59 -0400

    search and filter function working

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit, newItem, setNewItem}) {
+    
+  // setNewItem({
+  //     id: uuid(),
+  //     name: event.target.name.value,
+  //     category: event.target.category.value
+  //   })
+
+  
   return (
-    <form className="NewItem">
+    <form 
+    className="NewItem"
+    onSubmit={onItemFormSubmit}
+    >
       <label>
         Name:
         <input type="text" name="name" />
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,26 +2,79 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
+
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchContent, setSearch] = useState('')
+  const [newItem, setNewItem] = useState({
+    id: '',
+    name: '',
+    category: 'Produce'
+  })
+  const [newItemsArray, setItems] = useState(items)
+
+  console.log('newItem', newItem)
+  console.log('newItemArray', newItemsArray)
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearchChange(event) {
+    // console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+
+  const itemsToDisplay = newItemsArray.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  let searchedItems = ''
+  if (searchContent === '') {
+    searchedItems = [...itemsToDisplay]
+  }
+  else if (searchContent !== '') {
+    searchedItems = itemsToDisplay.filter((item) => {
+      if (item.name.includes(searchContent) || item.name.toLowerCase().includes(searchContent)) {
+        //console.log(item.name.toLowerCase())
+        return item
+      }
+    })
+  };
+  //console.log(searchContent === '')
+  //console.log(searchedItems)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(event.target.name.value, event.target.category.value)
+    setNewItem({
+      id: uuid(),
+      name: event.target.name.value,
+      category: event.target.category.value
+    })
+    setItems([...newItemsArray, newItem])
+  }
+
+  //The previous item is added when I try to add a second item, but the current item first registers as blank. Why?
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+      onItemFormSubmit={handleSubmit}
+      newItem={newItem}
+      setNewItem={setNewItem}
+      />
+      <Filter 
+      onCategoryChange={handleCategoryChange} 
+      onSearchChange={handleSearchChange}
+      search={searchContent}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchedItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
